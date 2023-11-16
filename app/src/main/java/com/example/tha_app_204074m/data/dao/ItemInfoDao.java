fb32c35e66c67369767326f18f25330e36526ad3
Upeksha Herath <upekshadilshan000@gmail.com>
2023-11-16 14:03:59 +0530

    initial commit

---
+++ app/src/main/java/com/example/tha_app_204074m/data/dao/ItemInfoDao.java
@@ -0,0 +1,28 @@
+package com.example.tha_app_204074m.data.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.tha_app_204074m.data.entity.ItemInfo;
+
+import java.util.List;
+
+@Dao
+public interface ItemInfoDao {
+    @Insert
+    void addItem(ItemInfo itemInfo);
+
+    @Query("Select * from Item_Infor")
+    LiveData<List<ItemInfo>> getAllItems();
+
+    @Update
+    void updateItem(ItemInfo itemInfo);
+
+    @Delete
+    void delete(ItemInfo itemInfo);
+
+}
---
+++ app/src/main/java/com/example/tha_app_204074m/utils/exceptions/AlertExceptions.java
@@ -0,0 +1,7 @@
+package com.example.tha_app_204074m.utils.exceptions;
+
+public class AlertExceptions extends Exception{
+    public AlertExceptions(String message){
+        super(message);
+    }
+}
